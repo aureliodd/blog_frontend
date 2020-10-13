e0af0933604ef48441deb7a2adfaf9197034579a
Aurelio D'Urso <aurelio@MBPdiAurelio2.homenet.telecomitalia.it>
2020-10-13 16:56:33 +0200

    ultime modifiche

--- app/Controller/postController.js
+++ app/Controller/postController.js
@@ -2,11 +2,12 @@ class PostController{
     
     constructor(){
         this.posts = [];
-        this.orderedposts = [];
+        this.orderedPosts = [];
         this.restController = new RestController();
         this.httpUrl = "http://localhost:3000/posts";
         this.modify = false;
         this.id_to_modify;
+        this.id_to_delete;
 
         //UI
         this.postContainer;
@@ -17,6 +18,7 @@ class PostController{
         this.userName;
         this.tags;
         this.modal;
+        this.blankPost
     }
 
     init(){
@@ -25,11 +27,10 @@ class PostController{
             this.orderPriorityCheckbox = $("#order-priority");
             this.newPostButton = $("#post");
             this.modal = $("#modal");
-
+            this.blankPost = $("#blanktext")
 
             this.orderPriorityCheckbox.change(function() {
-                this.posts.orderPosts();
-                this.checked ? this.posts.displayPosts(1) : this.posts.displayPosts(0);
+                this.orderPriorityCheckbox.prop("checked") ? this.displayPosts(1) : this.displayPosts(0);
             }.bind(this)); 
             
             this.newPostButton.click(function(){
@@ -51,54 +52,42 @@ class PostController{
         }.bind(this));
     }
 
-
-    addPostToArray(post){ this.posts.push(post); }
-
+    addPostToArray(post){ this.posts.push(post); this.orderedPosts.push(post) }
 
     getPosts(){
         this.restController.get(this.httpUrl,function(data,status,xhr){
             for(var element in data)
                 this.addPostToArray(new Post(data[element]._id,data[element].creator,data[element].title,data[element].content,data[element].priority,data[element].public,data[element].tags));
-                           
-            this.displayPosts(); //IMPORTANTISSIMO: se lo metto fuori a questa funzione anonima, essendo che è asincrono si rompe tutto il sito!
+            this.displayPosts();
         }.bind(this));
     }
 
-
     displayPosts(prior = 0){
+        this.postContainer.html("");
+        let aux_post;
 
-    this.postContainer.html("");
-    var newpost = null;
+        if(prior == 0) aux_post = this.posts
+        else aux_post = this.orderPosts();
 
-    if(prior == 0)
-        for(var post of this.posts){
-
-            newpost = this.addTextPost(post,0);
+        for(var post of aux_post){
+            let newpost = this.addTextPost(post);
                         
-            if(this.checkPriority(post.priority)) this.important(newpost);
-            if(!this.checkPublic(post.public_post)) this.privatePost(newpost);
+            if(post.priority > 0) this.important(newpost);
+            if(!post.public_post) this.privatePost(newpost);
 
             this.addPostToFeed(newpost);
-
 
             this.deleteBtn = $(".delete",newpost);
             this.modifyBtn = $(".modify",newpost);
 
-
-
             this.deleteBtn.click(function(event){
-
-                var div_id_to_delete = event.target.closest("section");
-                var id_to_delete = $(div_id_to_delete).data("identifier");        
-                
-                console.log(this.httpUrl +"/" + id_to_delete);
+                let div_id_to_delete = event.target.closest("section");
+                this.id_to_delete= $(div_id_to_delete).data("identifier");        
 
                 this.restController.delete(this.httpUrl + "/" + id_to_delete, function(){
-                    console.log("successo");
-                    location.reload();
+                    location.reload(); //success
                 });
             }.bind(this));
-
 
             this.modifyBtn.click(function(event){
                 this.modify = true;
@@ -106,44 +95,21 @@ class PostController{
                 this.id_to_modify  = $(div_id_to_modify).data("identifier");
                 this.openModal();
             }.bind(this));
-
         }
-        else 
-            for(var post of this.orderedposts){
-
-                newpost = this.addTextPost(post,0);
-                    
-                if(this.checkPriority(post.priority)) this.important(newpost);
-                if(!this.checkPublic(post.public_post)) this.privatePost(newpost);
-        
-                this.addPostToFeed(newpost);
-            }
     }
 
     CreateModifyPost(user, title, content, importnt, priv){
-
         var string_tags = $("#tags").val();
         var tags = string_tags.split(",");
 
-
         if(this.modify == false){
-
             let post_to_send = {creator: user, title: title, content: content, public: !priv, priority: Number(importnt), tags: tags };
-    
-
-            console.log(post_to_send);
-    
-        
-            var data = {
-                url: this.httpUrl,
-                crossDomain: true,
-                data: post_to_send,
-                dataType: "json",
+           
+            var data = { url: this.httpUrl, crossDomain: true, data: post_to_send, dataType: "json",
                 success: function(data,textStatus,jqXHR){
-                    console.log("data",data);
                     let post = new Post(data._id, data.creator, data.title, data.content, data.priority, data.public, tags);
                     this.addPostToArray(post);
-                    var newpost = this.addTextPost(post,1);
+                    var newpost = this.addTextPost(post);
                 
                     if(data.priority) this.important(newpost);
                     if(!data.public) this.privatePost(newpost);
@@ -153,165 +119,80 @@ class PostController{
                         this.comment(commentSection);
                     }.bind(this));
 
-                    console.log(newpost);
-
                     this.addPostToFeed(newpost);
             }.bind(this)};
 
             $.post(data);
         
         } else {
-
-                console.log(this.id_to_modify);
-
-                var post_to_modify = {
-                    title: title,
-                    content: content,
-                    public: !priv, //da cambiare con il riferimento alla checkbox
-                    priority: Number(importnt), // pure
-                    tags: tags,
-                 };
-    
+            var post_to_modify = { title: title, content: content, public: !priv, priority: Number(importnt), tags: tags };
+        
+            var data = {
+                url: this.httpUrl+ "/" + this.id_to_modify,
+                data: post_to_modify,
+                dataType: "json",
+                success: function(data,textStatus,jqXHR){console.log("data",data); }};
             
-                var data = {
-                    url: this.httpUrl+ "/" + this.id_to_modify,
-                    data: post_to_modify,
-                    dataType: "json",
-                    success: function(data,textStatus,jqXHR){
-                        console.log("data",data);
-                }};
-
-                console.log(data);
-                
-                this.restController.patch(data);
-                this.modify = false;
+            this.restController.patch(data);
+            this.modify = false;
         }
-
         this.hideModal();
         this.resetModal();
     }
 
-    addTextPost(post, isItMyPost){
-        var newpost = $("#blanktext").clone();
+    addTextPost(post){
+
+        var newpost = this.blankPost.clone();
         newpost.removeAttr("id");
 
+        $(newpost).data("identifier", post.id); //aggiungo identifier al dataset
 
-        $(newpost).data("identifier", post.id);
-        //console.log($(newpost).data()); //è il dataset
-
-        if(isItMyPost === 0) $(".creator",newpost).attr("href",post.creator + ".html");
-            else $(".creator",newpost).attr("href","profile.html");
-        if(post.creator != "") $(".creator",newpost).html(post.creator);
-            else $(".creator",newpost).html("Anonimo")
+        $(".creator",newpost).html(post.creator);
         $(".title",newpost).html(post.title);
         $(".content", newpost).html(post.content);
 
         //tags
-        var aux = "";
+        var tag_aux = "";
         if(post.tags != null){
             for(var tag of post.tags)
-            aux += " #"+ tag;
+                tag_aux += " #"+ tag;
 
-            $(".tags", newpost).html(aux);
+            $(".tags", newpost).html(tag_aux);
         }
-
 
         $(".send-comment",newpost).click(function(event){
             var commentSection = event.target.parentNode.parentNode;
-            //comment(commentSection);
-
             var name = $("#name").val() ? $("#name").val() : "Anonimo";
     
             var textarea = $(".write-comment", commentSection);
                 if(textarea.val()!== ""){
-                        var commentarea = $(".comment-area",commentSection.parentNode);
-        
-                        var newdiv = $('<div class="card py-1 px-2 my-2"><p> <span class="badge badge-primary">'+ name +'</span>  ' + textarea.val() + '</p></div>');
-            
-                        textarea.val("");
-                        if(commentarea.html()=="Nessun commento"){
-                            commentarea.html("");
-                            newdiv.appendTo(commentarea);
-                        } else 
-                            newdiv.appendTo(commentarea);
+                    var commentarea = $(".comment-area",commentSection.parentNode);
+                    var newdiv = $('<div class="card py-1 px-2 my-2"><p> <span class="badge badge-primary">'+ name +'</span>  ' + textarea.val() + '</p></div>');
+                    textarea.val("");
+
+                    if(commentarea.html()== "Nessun commento"){ commentarea.html(""); newdiv.appendTo(commentarea);}
+                    else newdiv.appendTo(commentarea);
                 }
         });
-    
         return newpost;
     }
 
-    
-    resetModal(){
-        $(this.titlePost).val("");
-        $(this.bodyPost).val("");
-    }
+    resetModal(){ $(this.titlePost).val(""); $(this.bodyPost).val(""); }
 
     openModal(){ $(this.modal).modal('show');}
 
     hideModal(){  $(this.modal).modal("hide"); }
     
-    checkPriority(priority){
-        if(priority == true) return 1
-        return 0;
-    }
-    
-    setPriority(checked){
-        if(checked == 1) return 1;
-        return 0;
-    }
+    important(content){ content.addClass("border border-primary"); }
 
-    checkPublic(public_post){
-        if(public_post == true) return 1
-        return 0;
-    }
-
-    setPrivate(checked){
-        if(checked == 1) return 1;
-        return 0;
-    }
-    
-    important(content){
-        content.addClass("border border-primary");
-    }
-
-    privatePost(content){
-        content.addClass("private-post");
-    }
+    privatePost(content){ content.addClass("private-post"); }
     
     addPostToFeed(newpost){
         this.show(newpost);
         this.postContainer.append(newpost);
     }
     
-    show(content){
-        content.addClass("visible");
-        content.removeClass("hide");
-    }
+    show(content){ content.addClass("visible"); content.removeClass("hide"); }
 
-    orderPosts(){
-        this.orderedposts = [];
-        for(var i = 0; i<this.posts.length;i++)
-            if(this.checkPriority(this.posts[i].priority)) this.orderedposts.push(this.posts[i]);
-        
-        for(var i = 0; i<this.posts.length;i++)
-            if(!this.checkPriority(this.posts[i].priority))  this.orderedposts.push(this.posts[i]);
-    }
-    
-    comment(commentSection){
-        var name = $("#name").val() ? $("#name").val() : "Anonimo";
-    
-        var textarea = $(".write-comment", commentSection);
-            if(textarea.val()!== ""){
-                    var commentarea = $(".comment-area",commentSection.parentNode);
-    
-                    var newdiv = $('<div class="card py-1 px-2 my-2"><p> <span class="badge badge-primary">'+ name +'</span>  ' + textarea.val() + '</p></div>');
-        
-                    textarea.val("");
-                    if(commentarea.html()=="Nessun commento"){
-                        commentarea.html("");
-                        newdiv.appendTo(commentarea);
-                    } else 
-                        newdiv.appendTo(commentarea);
-            }
-    }
+    orderPosts(){ return this.orderedPosts.sort(function(a,b) {return b.priority - a.priority;}); } //descending order
 }
